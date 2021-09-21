00f38976cf832de4a6fb950f02853c1f32101060
mluizaa00 <taeckls@gmail.com>
2021-09-21 15:53:52 -0300

    update(main): while to for, better code-reading and limit of number input

--- moduleOne/main.py
+++ moduleOne/main.py
@@ -5,15 +5,26 @@ secret_number = 20;
 total_chances = 10;
 current_chance = 1;
 
-while (current_chance <= total_chances):
-    print("Available chances: " + str(current_chance))
-    number = int(input("Type your number: "));
+minimum_number = 1;
+maximum_number = 100;
+
+limit = "{} to {}", minimum_number, maximum_number;
+
+for current_chance in range(1, total_chances):
+    print("Available chances: {}", str(current_chance))
+    number = int(input("Type your number between {}: ", limit));
+
+    if (minimum_number < 1 | maximum_number > 100):
+        print("You can only print numbers between {}!", limit)
+        continue;
 
     if (number == secret_number):
         print("You typed the right number!")
-    elif (number > secret_number):
+        break;
+
+    if (number > secret_number):
         print("You typed the wrong number! The number typed is higher than the secret number.")
     else:
         print("You typed the wrong number! The number typed is lower than the secret number.")
 
-    current_chance = current_chance + 1;
+print("The game was finished!")
