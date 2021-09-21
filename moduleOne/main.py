cfde5358f21bcfb913b19efdca62f1d973915d67
mluizaa00 <taeckls@gmail.com>
2021-09-21 16:41:26 -0300

    feat(main): added user points variable to calc final points on game

--- moduleOne/main.py
+++ moduleOne/main.py
@@ -2,22 +2,30 @@ import random;
 
 print("Welcome to the Game!")
 
-difficulty_rates = set([3, 5, 8])
+difficulty_rates = list([8, 5, 3])
+
+start_points = list([100, 60, 50])
+lose_points = list([10, 15, 20])
 
 print("[1] Easy [2] Medium [3] Hard")
-difficulty = int(input("Type your difficulty level: ")) + 1;
+difficulty = int(input("Type your difficulty level: "));
 
 if (difficulty < 1 or difficulty > 3):
     difficulty = 1;
     print("You typed a wrong difficulty! Using Easy as default.");
 
-total_chances = list(difficulty_rates)[difficulty - 1];
-print("You selected the difficult {}, you have a total of {} chances".format(difficulty, total_chances))
+difficulty = difficulty - 1;
+
+total_chances = difficulty_rates[difficulty];
+print("You selected the difficult {}, you have a total of {} chances".format(difficulty + 1, total_chances))
 
 current_round = 1;
 
 minimum_number = 1;
 maximum_number = 100;
+
+current_points = start_points[difficulty]
+points_to_lose = lose_points[difficulty]
 
 secret_number = int(random.randrange(minimum_number, maximum_number));
 
@@ -40,5 +48,8 @@ for current_round in range(1, total_chances):
     else:
         print("You typed the wrong number! The number typed is lower than the secret number.")
 
+    current_points = current_points - points_to_lose;
+
 print("The game was finished!");
+print("Final points: {}".format(current_points))
 print("The secret number is {}".format(secret_number));
