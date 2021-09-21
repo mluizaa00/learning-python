f77b15dc867c36b611dca19ab369b70a92d11a91
mluizaa00 <taeckls@gmail.com>
2021-09-21 16:33:32 -0300

    feat(main): add random secret number and difficulty rate to game

--- moduleOne/main.py
+++ moduleOne/main.py
@@ -1,21 +1,34 @@
+import random;
+
 print("Welcome to the Game!")
 
-secret_number = 20;
+difficulty_rates = set([3, 5, 8])
 
-total_chances = 10;
-current_chance = 1;
+print("[1] Easy [2] Medium [3] Hard")
+difficulty = int(input("Type your difficulty level: ")) + 1;
+
+if (difficulty < 1 or difficulty > 3):
+    difficulty = 1;
+    print("You typed a wrong difficulty! Using Easy as default.");
+
+total_chances = list(difficulty_rates)[difficulty - 1];
+print("You selected the difficult {}, you have a total of {} chances".format(difficulty, total_chances))
+
+current_round = 1;
 
 minimum_number = 1;
 maximum_number = 100;
 
-limit = "{} to {}", minimum_number, maximum_number;
+secret_number = int(random.randrange(minimum_number, maximum_number));
 
-for current_chance in range(1, total_chances):
-    print("Available chances: {}", str(current_chance))
-    number = int(input("Type your number between {}: ", limit));
+limit = "{} to {}".format(minimum_number, maximum_number);
 
-    if (minimum_number < 1 | maximum_number > 100):
-        print("You can only print numbers between {}!", limit)
+for current_round in range(1, total_chances):
+    print("Current round: {}".format(str(current_round)))
+    number = int(input("Type your number between {}: ".format(limit)));
+
+    if (number < minimum_number or number > maximum_number):
+        print("You can only print numbers between {}! ".format(limit))
         continue;
 
     if (number == secret_number):
@@ -27,4 +40,5 @@ for current_chance in range(1, total_chances):
     else:
         print("You typed the wrong number! The number typed is lower than the secret number.")
 
-print("The game was finished!")
+print("The game was finished!");
+print("The secret number is {}".format(secret_number));
