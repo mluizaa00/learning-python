13c5494250c1860c758a26fdd5893acb2c370fd5
mluizaa00 <taeckls@gmail.com>
2021-10-07 13:28:00 -0300

    update(hangman_game): added points, chances and better code reading

--- alura/hangman_game.py
+++ alura/hangman_game.py
@@ -1,23 +1,43 @@
+import random;
+import game_controller;
+
 won = False;
 lose = False;
 
 words = list(["laila", "luke", "preta", "gigi"])
+secret_word = words[random.randrange(0, 4)].lower();
+
+positions_found = list();
+
+chances = 5;
+points = 0;
 
 def start_game():
     while(not won and not lose):
-        alternative = input("What letter will you choose? ");
-        secret_word = words[0]
-
-        index = 1;
+        alternative = input("What letter will you choose? ").lower().strip();
         right_guess = False;
         
         for letter in secret_word:
             if (alternative == letter):
-                print("You got it right! It's letter {} in position {}.".format(letter, index))
-                index = index + 1;
-                right_guess = True;
+                right_guess = handle_correct(letter)
                 break;
         
         if (right_guess == False):
-            print("You didn't got it right! Try again...")
+            handle_incorrect();
+
+        if (chances == 0):
+            game_controller.finish_game(points);
+            return;
+
+def handle_correct(letter):
+    print("You got it right! It's letter {} in position {}.".format(letter, secret_word.find(letter) + 1));
+
+    global points;
+    points = points + 1;
+    return True;
+
+def handle_incorrect():
+    print("You didn't got it right! Try again...")
+    global chances;
+    chances = chances - 1;
             
